0b6c0cf5857f49f64a3a7175ab5d518c027df9a8
RefotsirkNged <kristoffer@hylleby.dk>
2016-01-31 02:54:12 +0100

    asdsadasasdds

--- BankoProject/Models/BingoCard/BingoCard.cs
+++ BankoProject/Models/BingoCard/BingoCard.cs
@@ -8,6 +8,29 @@ namespace BankoProject.Models
 {
   class BingoCard
   {
+    private BingoCardColumns _columns;
+    private BingoCardRows _rows;
 
+
+    public BingoCard()
+    {
+
+    }
+
+    public BingoCardColumns Columns
+    {
+      get
+      {
+        return _columns;
+      }
+    }
+
+    public BingoCardRows Rows
+    {
+      get
+      {
+        return _rows;
+      }
+    }
   }
 }
---
+++ BankoProject/Models/BingoCard/BingoCardRows.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankoProject.Models
+{
+  class BingoCardRows
+  {
+    #region Fields
+    private int[] _row1;
+    private int[] _row2;
+    private int[] _row3;
+
+    private List<int[]> _rows;
+    #endregion
+
+
+    public BingoCardRows(int[][] Rows)
+    {
+      _row1 = Rows[0];
+      _rows.Add(Row1);
+      _row2 = Rows[1];
+      _rows.Add(Row2);
+      _row3 = Rows[2];
+      _rows.Add(Row3);
+    }
+    #region Methods
+    public int Count()
+    {
+      int count = 0;
+      foreach (var row in _rows)
+      {
+        for (int i = 0; i < 9; i++)
+        {
+          if (row[i] != 0)
+          {
+            count++;
+          }
+        }
+      }
+      return count;
+    }
+    #endregion
+    #region Accessors
+    public int[] Row1
+    {
+      get
+      {
+        return _row1;
+      }
+    }
+
+    public int[] Row2
+    {
+      get
+      {
+        return _row2;
+      }
+    }
+
+    public int[] Row3
+    {
+      get
+      {
+        return _row3;
+      }
+    }
+    #endregion
+  }
+}
