1f2017047e7060d61e72da7e6d52d7a2cc160ba2
Reffotsirk Nged <kristoffer@hylleby.dk>
2016-03-05 18:22:22 +0100

    stuff was shiz

--- AlgorithmTester/Program.cs
+++ AlgorithmTester/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using BankoProject;
+using BingoCardGenerator;
 
 namespace AlgorithmTester
 {
@@ -12,6 +14,17 @@ namespace AlgorithmTester
     {
         static void Main(string[] args)
         {
+            Generator gen = new Generator("");
+            var resultingcard = gen.GenerateCard();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(resultingcard[j, i] + "   ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         } 
     }
 }
