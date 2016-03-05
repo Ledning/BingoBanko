3e29e5cfd46d3c5c76291fffd1f668458397f4ea
Kristoffer Degn <kristoffer@hylleby.dk>
2016-03-05 17:21:02 +0100

    MakeTokensGreatAgain2k16

---
+++ AlgorithmTester/MakeIntToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AlgorithmTester
+{
+  public static class TokenExtentions
+  {
+    public static int MakeInt(this String str)
+    {
+      try
+      {
+        char[] charArray;
+        str = str.ToLower();
+        long longChecker = 0;
+        int finalNumba = 0;
+        string stringChecker = "";
+        Regex regx = new Regex("([a-zæøå0-9])+");
+        if(regx.IsMatch(str))
+        {
+          charArray = str.ToCharArray();
+          foreach (char item in charArray)
+          {
+            stringChecker += (Convert.ToInt64(item)).ToString();
+            if (Convert.ToInt64(stringChecker) >= Int32.MaxValue - 1000)
+            {
+              longChecker = Convert.ToInt64(stringChecker) % (Int32.MaxValue - 1000);
+              stringChecker = Convert.ToString(longChecker);
+            }
+          }
+          
+        }
+        return finalNumba = Convert.ToInt32(stringChecker);
+      }
+      catch (ArgumentException e)
+      {
+        throw e;
+      }
+     
+      
+    }
+  }
+}
