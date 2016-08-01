15310b26fb3f90f0e0e5acee44dac30932ebc49c
Reffotsirk Nged <kristoffer@hylleby.dk>
2016-08-01 22:32:44 +0200

    fdg fgd

--- BankoProject/Models/BingoEvent.cs
+++ BankoProject/Models/BingoEvent.cs
@@ -8,5 +8,24 @@ namespace BankoProject.Models
 {
   class BingoEvent
   {
+
+
+    //names, dates, general stuff
+    private string _title;
+    private DateTime _startDateTime;
+
+    //flags (has seed been manipulated, what was original seed, technical stuff
+    private bool _seedManipulated; //basically isDirty
+    private string _seed;//what is the seed rn? might have changed
+    private string _originalSeed; // generated based on event-name, then fed into algorithm
+
+
+    //any aggregated objects; settings object(general/specific), lists of objects for the competitions held during the event, 
+    //private List<competitionObject> _compettionList;
+
+
+
+
+
   }
 }
