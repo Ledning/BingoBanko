6f5d29ba622681956cdf216b60ce1ee340461dbd
Reffotsirk Nged <kristoffer@hylleby.dk>
2016-08-04 20:41:33 +0200

    shieet

---
+++ BankoProject/ViewModels/VisualSnaskViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BankoProject.Tools;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankoProject.ViewModels
+{
+  class VisualSnaskViewModel : Screen, IMainViewItem
+  {
+
+
+
+
+
+
+  }
+}
