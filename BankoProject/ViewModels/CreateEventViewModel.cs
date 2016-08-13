3af9c10dbf6efbca4b9db65259f1f37959215bcd
Mathias Leding <mledin14@student.aau.dk>
2016-08-13 12:42:04 +0200

    CreateEvent GUI

--- BankoProject/ViewModels/CreateEventViewModel.cs
+++ BankoProject/ViewModels/CreateEventViewModel.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankoProject.Models;
+using Caliburn.Micro;
 
 namespace BankoProject.ViewModels
 {
   class CreateEventViewModel
   {
+    private BingoEvent _bingoEvent;
+
     public CreateEventViewModel()
     {
       throw new NotImplementedException();
+
+
+
+
+
+    }
+    protected override void OnViewReady(object view)
+    {
+      _bingoEvent = IoC.Get<BingoEvent>();
     }
   }
 }
