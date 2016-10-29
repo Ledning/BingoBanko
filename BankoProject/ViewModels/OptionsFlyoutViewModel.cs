1625e0b15a19e1787cdde4cc9b041742241e236f
Reffotsirk Nged <kristoffer@hylleby.dk>
2016-10-29 13:14:13 +0200

    noice

--- BankoProject/ViewModels/OptionsFlyoutViewModel.cs
+++ BankoProject/ViewModels/OptionsFlyoutViewModel.cs
@@ -4,10 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankoProject.Tools;
 
 namespace BankoProject.ViewModels
 {
+  /// <summary>
+  /// lul
+  /// </summary>
   public class OptionsFlyoutViewModel : Screen
   {
+
+    public BindableCollection<IEnumerable<Monitor>> MonitorList { get; set; }
+    private readonly ILog _log = LogManager.GetLog(typeof(MainWindowViewModel));
+
+    public OptionsFlyoutViewModel()
+    {
+
+    }
+
+    protected override void OnViewReady(object view)
+    {
+      MonitorList = new BindableCollection<IEnumerable<Monitor>>();
+      MonitorList.Add(Monitor.AllMonitors);
+      _log.Info("TRIGGERED");
+    }
+
+
+
+
   }
 }
