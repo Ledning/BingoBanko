181af25d47d4bd95db4813dc085e5dca9ef62ec0
Reffotsirk Nged <kristoffer@hylleby.dk>
2016-11-13 20:00:54 +0100

    qweqwe

--- BankoProject/ViewModels/PresentationScreenViewModels/FullscreenImageViewModel.cs
+++ BankoProject/ViewModels/PresentationScreenViewModels/FullscreenImageViewModel.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BankoProject.Tools;
+using Caliburn.Micro;
 
 namespace BankoProject.ViewModels.PresentationScreenViewModels
 {
   class FullscreenImageViewModel : IPresentationScreenItem
   {
     public ApplicationWideEnums.AspectRatio AsRatio { get; set; }
+
+
+    /// <summary>
+    /// Contains a list of all the different pictures to be shown
+    /// </summary>
+    public BindableCollection<string> ImageList { get; set; }
   }
 }
---
+++ BankoProject/ViewModels/PresentationScreenViewModels/PresentationScreenHostViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankoProject.Tools;
+using Caliburn.Micro;
+
+namespace BankoProject.ViewModels.PresentationScreenViewModels
+{
+  /// <summary>
+  /// This is where you would set up all the shit, so when this is put up, the rest follows.
+  /// </summary>
+  class PresentationScreenHostViewModel : Conductor<IPresentationScreenItem>.Collection.OneActive
+  {
+
+
+
+
+
+    public void ShowFullscreenImage()
+    {
+      ActiveItem
+    }
+
+
+
+  }
+}
