12d75de79115eedd71ed509ddd2a05acab8a18fb
Dascham <sydowtwenty@gmail.com>
2017-02-04 19:07:45 +0100

    Added some classes for managing the teams and stuff

--- BankoProject/ViewModels/ControlPanelViewModel.cs
+++ BankoProject/ViewModels/ControlPanelViewModel.cs
@@ -2,6 +2,7 @@
 using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Dynamic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 using BankoProject.Models;
 using BankoProject.Tools.Events;
 using BankoProject.ViewModels.PresentationScreen;
+using Catel.Collections;
 
 namespace BankoProject.ViewModels
 {
@@ -30,6 +32,7 @@ namespace BankoProject.ViewModels
     {
       BoardVM = new BoardViewModel();
       ActivateItem(BoardVM);
+      
     }
 
     protected override void OnViewReady(object view)
@@ -92,6 +95,10 @@ namespace BankoProject.ViewModels
       set { _contestDuration = value; NotifyOfPropertyChange(() => ContestDuration);}
     }
 
+    private ObservableCollection<Team> _allTeams; 
+    public ObservableCollection<Team> AllTeams { get { return _allTeams; } set { _allTeams = value; NotifyOfPropertyChange(()=> AllTeams);} } 
+
+
     public void ShowLatestNumbers()
     {
       throw new NotImplementedException();
@@ -138,7 +145,11 @@ namespace BankoProject.ViewModels
 
     public void StartContest()
     {
-      throw new NotImplementedException();
+      CompetitionObject competition = new CompetitionObject(this.NumberOfContestants, this.NumberOfTeams, this.ContestDuration);
+
+      this.AllTeams = new ObservableCollection<Team>(competition.AllTeams);
+      
+      //this should prolly also start some counter somewhere
     }
 
 
